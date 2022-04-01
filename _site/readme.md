3645e845267a122858535c7d7940a79410fe882d
Eyon42 <francescogentyle@gmail.com>
2022-04-01 01:28:44 -0300

    Re-added _site

---
+++ _site/readme.md
@@ -0,0 +1,12 @@
+# This is my blog made with jekyll
+
+## To-Do:
+    - Add content
+
+## Run instructions
+```
+$ gem install bundler jekyll
+$ bundle install
+$ npm install // for tailwind css
+$ bundle exec jekyll serve
+```
---
+++ _site/tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    //mode: "jit",
+    purge: [
+        './_includes/**/*.html',
+        './_layouts/**/*.html',
+        './_posts/*.md',
+        './*.html',
+    ],
+    darkMode: false,
+    theme: {
+        fontFamily: {
+            'sans': ['Open Sans']
+        },
+        extend: {
+            colors: {
+                primaryRed: "#d64937",
+                strongRed: "#dc240c",
+                secondaryRed: "#df7568",
+                white0: "#ffffff",
+                white1: "#f8f9fa",
+                white2: "#ebebeb",
+                white3: "#C0C0C4",
+                black0: "#131516",
+                black1: "#212529",
+                black2: "#4F4F54",
+            },
+            height: {
+                "screen75": "75vh",
+                "screen90": "90vh",
+                "screen/2": "50vh",
+                "screen/3": "calc(100vh / 3)",
+
+            },
+            transitionDuration: {
+                '0': '0ms',
+                '2500': '2500ms',
+                '5000': '5000ms',
+            }
+        },
+    },
+    variants: {},
+    safelist: [
+        "text-black0", "text-[#5E7BA7]", "text-[#F0DA50]", "text-[#505050]"
+    ],
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
+}
